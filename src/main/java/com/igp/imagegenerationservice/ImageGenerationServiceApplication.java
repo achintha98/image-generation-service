123c5dfc67e9d34faedb9fb19d02e780fb4f3dc3
achintha98 <kalanaachintha@gmail.com>
2025-05-15 10:59:27 +0530

    init igp project

---
+++ src/main/java/com/igp/imagegenerationservice/ImageGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.igp.imagegenerationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageGenerationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageGenerationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/igp/imagegenerationservice/ImageGenerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igp.imagegenerationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGenerationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

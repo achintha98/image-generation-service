1e28e23be8914c29e4aa3ae52e641d68bcdf0930
achintha98 <kalanaachintha@gmail.com>
2025-05-20 21:24:49 +0530

    Fixed return type issue in pack controller's generatePackImages endpoint

--- src/main/java/com/igp/imagegenerationservice/controller/PackController.java
+++ src/main/java/com/igp/imagegenerationservice/controller/PackController.java
@@ -2,11 +2,16 @@ package com.igp.imagegenerationservice.controller;
 
 import com.igp.imagegenerationservice.dto.ModelRequestDTO;
 import com.igp.imagegenerationservice.dto.ModelResponseDTO;
+import com.igp.imagegenerationservice.dto.OutputImageResponseDTO;
+import com.igp.imagegenerationservice.model.Model;
 import com.igp.imagegenerationservice.service.PackService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Achintha Kalunayaka
@@ -24,8 +29,10 @@ public class PackController {
     }
 
     @PostMapping(path = "/generate/{packId}")
-    public ResponseEntity<ModelResponseDTO> generatePackImages(@PathVariable String packId) {
-        ModelResponseDTO modelResponseDTO = modelService.createModel(modelRequestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(modelResponseDTO);
+    public ResponseEntity<List<OutputImageResponseDTO>> generatePackImages(@PathVariable UUID packId, @RequestBody Model model) {
+        List<OutputImageResponseDTO> outputImageResponseDTO = packService.generatePack(packId, model);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(outputImageResponseDTO);
     }
 }

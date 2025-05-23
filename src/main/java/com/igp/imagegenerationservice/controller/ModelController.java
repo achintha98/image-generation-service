3df656586fe3fe2a1753f2ff6947b8d2c22e3578
achintha98 <kalanaachintha@gmail.com>
2025-05-23 18:54:58 +0530

    Solved cors issue on model controller

--- src/main/java/com/igp/imagegenerationservice/controller/ModelController.java
+++ src/main/java/com/igp/imagegenerationservice/controller/ModelController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = "/")
 public class ModelController {
 

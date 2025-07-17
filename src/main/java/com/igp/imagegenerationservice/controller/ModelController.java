a54e1c3f082746515f1fde542e09dfc43c4c48cc
achintha98 <kalanaachintha@gmail.com>
2025-07-17 23:29:34 +0530

    Added public model creation

--- src/main/java/com/igp/imagegenerationservice/controller/ModelController.java
+++ src/main/java/com/igp/imagegenerationservice/controller/ModelController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Achintha Kalunayaka
@@ -22,38 +23,40 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = "/")
 public class ModelController {
-
     private final ModelService modelService;
-
     private final CloudService cloudService;
     private final ImageService imageService;
-
     public ModelController(ModelService modelService, ImageService imageService, CloudService cloudService) {
         this.imageService = imageService;
         this.modelService = modelService;
         this.cloudService = cloudService;
     }
-
     @PostMapping (path = "ai/training")
-    public ResponseEntity<ModelResponseDTO> createModel(@Validated @AuthenticationPrincipal Jwt jwt, @RequestBody ModelRequestDTO modelRequestDTO) {
-        String clerkUserId = jwt.getSubject();
-        modelRequestDTO.setUserId(clerkUserId);
+    public ResponseEntity<ModelResponseDTO> createModel(@Validated @RequestBody ModelRequestDTO modelRequestDTO) {
+//        String clerkUserId = jwt.getSubject();
+//        modelRequestDTO.setUserId(clerkUserId);
         ModelResponseDTO modelResponseDTO = modelService.createModel(modelRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(modelResponseDTO);
     }
 
-    @PostMapping (path = "ai/generate")
-    public ResponseEntity<OutputImageResponseDTO> generateImage(@Validated @RequestBody OutputImageRequestDTO outputImageRequestDTO) {
-        OutputImageResponseDTO outputImageResponseDTO = imageService.generateImage(outputImageRequestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(outputImageResponseDTO);
+    @PostMapping (path = "ai/public/training")
+    public ResponseEntity<ModelResponseDTO> createPublicModel(@Validated @RequestBody ModelRequestDTO modelRequestDTO) {
+//        String clerkUserId = jwt.getSubject();
+//        modelRequestDTO.setUserId(clerkUserId);
+        ModelResponseDTO modelResponseDTO = modelService.createPublicModel(modelRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(modelResponseDTO);
     }
 
+    @PostMapping (path = "ai/generate")
+    public ResponseEntity<OutputImageResponseDTO> generateImage(@RequestBody Map<String, String> payload) {
+        OutputImageResponseDTO outputImageResponseDTO = imageService.generateImage(payload.get("prompt"), payload.get("selectedModel"));
+        return ResponseEntity.status(HttpStatus.CREATED).body(outputImageResponseDTO);
+    }
     @GetMapping (path = "ai/presign-url")
     public ResponseEntity<PreSignResponseDTO> getPreSignURL() {
         PreSignResponseDTO preSignResponse = cloudService.createPreSignUrl();
         return ResponseEntity.status(HttpStatus.OK).body(preSignResponse);
     }
-
     @GetMapping (path = "ai/models")
     public ResponseEntity<List<ModelResponseDTO>> getModelList() {
         List<ModelResponseDTO> modelList = modelService.getModelList();

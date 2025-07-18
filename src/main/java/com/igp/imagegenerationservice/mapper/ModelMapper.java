b0a739b410d03c9364b742ced389896809423201
achintha98 <kalanaachintha@gmail.com>
2025-07-18 22:12:26 +0530

    Updated ModelMapper

--- src/main/java/com/igp/imagegenerationservice/mapper/ModelMapper.java
+++ src/main/java/com/igp/imagegenerationservice/mapper/ModelMapper.java
@@ -4,8 +4,6 @@ import com.igp.imagegenerationservice.dto.ModelRequestDTO;
 import com.igp.imagegenerationservice.dto.ModelResponseDTO;
 import com.igp.imagegenerationservice.model.Model;
 
-import java.time.LocalDate;
-
 /**
  * @author Achintha Kalunayaka
  * @since 5/16/2025
@@ -14,12 +12,12 @@ public class ModelMapper {
     public static ModelResponseDTO mapToModelResponseDTO(Model model) {
         return ModelResponseDTO.builder().
                 id(model.getId().toString()).name(model.getName()).
-                age(model.getAge()).gender(model.getGender()).
+                age(model.getAge()).gender(model.getGender()).zipUrl(model.getZipUrl()).isOpen(model.getIsOpen()).
                 ethnicity(model.getEthnicity()).eyeColor(model.getEyeColor()).thumbnail(model.getThumbnail()).build();
     }
 
     public static Model mapFromModelRequestDTO(ModelRequestDTO modelRequestDTO) {
-        return Model.builder().name(modelRequestDTO.getName()).ethnicity(modelRequestDTO.getEthnicity()).
+        return Model.builder().name(modelRequestDTO.getName()).ethnicity(modelRequestDTO.getEthnicity()).zipUrl(modelRequestDTO.getZipUrl()).
                 eyeColor(modelRequestDTO.getEyeColor()).isBald(modelRequestDTO.isBald()).gender(modelRequestDTO.getGender()).
                 isOpen(modelRequestDTO.getIsOpen()).thumbnail(modelRequestDTO.getThumbnail()).userId(modelRequestDTO.getUserId()).build();}
 }

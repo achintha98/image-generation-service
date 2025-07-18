722909dddc9dcb06c4bcbd57a0b7d0177c33730a
achintha98 <kalanaachintha@gmail.com>
2025-07-18 22:10:47 +0530

    Updated webhook endpoints

--- src/main/java/com/igp/imagegenerationservice/controller/WebhookController.java
+++ src/main/java/com/igp/imagegenerationservice/controller/WebhookController.java
@@ -1,42 +1,47 @@
 package com.igp.imagegenerationservice.controller;
 
 import ai.fal.client.Output;
-import ai.fal.client.queue.QueueStatus;
 import com.google.gson.JsonObject;
-import com.igp.imagegenerationservice.dto.ModelRequestDTO;
 import com.igp.imagegenerationservice.dto.ModelResponseDTO;
 import com.igp.imagegenerationservice.dto.OutputImageResponseDTO;
-import com.igp.imagegenerationservice.service.ImageGenerateService;
+import com.igp.imagegenerationservice.service.ImageService;
 import com.igp.imagegenerationservice.service.ModelService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author Achintha Kalunayaka
  * @since 5/21/2025
  */
+@RestController
+@RequestMapping(path = "/")
 public class WebhookController {
 
     private final ModelService modelService;
-    private final ImageGenerateService imageGenerateService;
+    private final ImageService imageService;
 
-    public  WebhookController(ModelService modelService, ImageGenerateService imageGenerateService) {
-        this.imageGenerateService = imageGenerateService;
+    public  WebhookController(ModelService modelService, ImageService imageService) {
+        this.imageService = imageService;
         this.modelService = modelService;
     }
 
     @PostMapping(path = "fal-ai/webhook/training")
-    public ResponseEntity<ModelResponseDTO> trainModel(@RequestBody Output<JsonObject> objectOutput) {
-        ModelResponseDTO modelResponseDTO = modelService.updateModel(objectOutput);
-        return ResponseEntity.status(HttpStatus.CREATED).body(modelResponseDTO);
+    public ResponseEntity<ModelResponseDTO> trainModel(@RequestBody Map<String, Object> payload) {
+        String status = (String) payload.get("status");
+        Map<String, Object> nested = (Map<String, Object>) payload.get("payload");
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping(path = "fal-ai/webhook/generate")
-    public ResponseEntity<OutputImageResponseDTO> generateImage( @RequestBody Output<JsonObject> objectOutput) {
-        OutputImageResponseDTO outputImageResponseDTO = imageGenerateService.updateImage(objectOutput);
-        return ResponseEntity.status(HttpStatus.CREATED).body(outputImageResponseDTO);
+    public ResponseEntity<OutputImageResponseDTO> generateImage( @RequestBody Map<String, Object> payload) {
+        String status = (String) payload.get("status");
+        Map<String, Object> nested = (Map<String, Object>) payload.get("payload");
+        return ResponseEntity.ok().build();
     }
 }
